46cd7b24c618c825e3b1e233609f1d668c930aa8
wdrf14 <weidermartenn@gmail.com>
2025-02-18 20:36:49 +0300

    asdasd

--- src/pages/main/ui/Main.vue
+++ src/pages/main/ui/Main.vue
@@ -4,7 +4,7 @@
         <div class="min-w-full flex flex-col items-center gap-10 p-4 mb-14" v-if="isOnline">
             <!-- Приветственный блок -->
             <div class="flex flex-col justify-center items-center w-[44rem] h-[12rem] bg-zinc-900 rounded-md gap-6">
-                <span class="text-xl text-white">Добро пожаловать на FKanban</span>
+                <span class="text-xl text-white">Добро пожаловать на FKanbannnnnnnn</span>
                 <span class="text-center text-gray-300">Ваш идеальный инструмент для организации задач и управления проектами.</span>
                 <button @click="createProject">Создать проект</button>
             </div>
